78edd862296510a65a6e66ff9d91bbf1fe4a3066
edudins <edgars@dudins.lv>
2025-08-10 07:32:50 +0300

    Experiment by adding different geometry

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { KeyboardService } from './services/keyboard_service';
 import { Being } from './objects/being';
-import { ArcadePhysics } from './engines/physics_engine';
+import { ArcadePhysics, RealisticPhysics } from './engines/physics_engine';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -17,14 +17,19 @@ renderer.setSize(0.8 * window.innerWidth, 0.8 * window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
+const geometry2 = new THREE.CircleGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material2 = new THREE.MeshBasicMaterial({ color: 0xf0ff0f });
 const cube = new THREE.Mesh(geometry, material);
+const circle = new THREE.Mesh(geometry2, material2)
 scene.add(cube);
+scene.add(circle);
 camera.position.z = 5;
 camera.position.y = 0.8;
 
 const keyboard = new KeyboardService();
-const player = new Being(cube, new ArcadePhysics(2));
+const player1 = new Being(cube, new RealisticPhysics(10));
+const player2 = new Being(circle, new ArcadePhysics(5));
 
 const clock = new THREE.Clock();
 
@@ -33,7 +38,8 @@ function animate() {
   const deltaTime = clock.getDelta();
 
   const input = keyboard.getState();
-  player.update(input, deltaTime);
+  player1.update(input, deltaTime);
+  player2.update(input, deltaTime);
 
   renderer.render(scene, camera);
 }
